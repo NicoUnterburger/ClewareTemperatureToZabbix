72437acab4a95f26ffcceadd2de50901440a6165
Nico Unterburger <nico-unterburger@live.de>
2023-06-10 13:13:42 +0200

    Update cleware-wrapper.sh

--- cleware-wrapper.sh
+++ cleware-wrapper.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Script for Monitoring a Cleware Temperature-Sensor with Zabbix
-# created 2023-06-05
+# created 2023-06-05 by github.com/NicoUnterburger
 
 temp=$(/etc/cleware/USBtemp -b)
 temp2=${temp::-2}
